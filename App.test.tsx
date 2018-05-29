650e8e1ab4777432a39c22f9d538b18acfad4398
Jen Garcia <jen@cuibonobo.com>
2018-05-29 10:41:14 -0400

    Initial commit

---
+++ App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import App from './src/AppRoot';
+
+import renderer from 'react-test-renderer';
+
+it('renders without crashing', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
